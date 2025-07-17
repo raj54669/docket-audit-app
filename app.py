fbf17b651a929bde76b60a3c7e9578db18063a74
raj54669 <raj.54669@gmail.com>
2025-07-17 12:53:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -154,7 +154,7 @@ if not models:
     st.error("❌ No models found in data.")
     st.stop()
 
-col1, col2, col3 = st.columns([3.2, 1.3, 3.5])
+col1, col2, col3 = st.columns([3.2, 1.5, 4])
 
 with col1:
     model = st.selectbox("🚙 Model", models)
