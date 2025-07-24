019f1f83b5d10d65638433de18c2193ea1c14097
Nishal Modi <raj.54669@gmail.com>
2025-07-24 18:31:00 +0530

    Update app-PV.py

--- app-PV.py
+++ app-PV.py
@@ -303,7 +303,7 @@ def render_combined_table(row, shared_fields, grouped_fields, group_keys):
     return html
 
 # --- Output ---
-st.markdown(f"<h2 style='margin-top: 0px; '> 🚙 {model} - {fuel_type} - {variant}</h2>", unsafe_allow_html=True)
+st.markdown(f"<h2 style='margin-top: -8px; '> 🚙 {model} - {fuel_type} - {variant}</h2>", unsafe_allow_html=True)
 st.markdown("<h3 style='color:#e65100; margin-top: -14px; margin-bottom: -8px;'>📝 Vehicle Pricing Details</h3>", unsafe_allow_html=True)
 
 shared_fields = [
