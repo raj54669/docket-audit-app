877bd9f0f0b43e3412c2317b0b1d2b1050abf2ed
Nishal Modi <raj.54669@gmail.com>
2026-01-11 13:55:54 +0530

    Update test.py

--- test.py
+++ test.py
@@ -25,7 +25,7 @@ st.markdown("""
     --label-size: 14px;
     --select-font-size: 15px;
     --table-font-size: 14px;
-    --variant-title-size: 24px;
+    --variant-title-size: 18px;
 }
 .block-container { padding-top: 0rem; }
 header {visibility: hidden;}
